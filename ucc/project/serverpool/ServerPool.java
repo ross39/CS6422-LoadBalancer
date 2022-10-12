94b5018c945d2db508b6880f8d03ab5d4a11f656
rossheaney <Rheaney26@gmail.com>
2022-10-12 14:44:35 +0100

    initial setup of ServerPool. Using package ucc.project again. I have added comments to help me understand what I need to do

---
+++ ucc/project/serverpool/ServerPool.java
@@ -0,0 +1,25 @@
+package ucc.project.serverpool;
+
+public class ServerPool {
+
+    public void createServerPool(){
+        //Will create an empty hashmap containing key, server
+
+    }
+
+    public void listenForServer(){
+        //Anytime a new server is created, call updateServerList() to update the hashmap
+
+    }
+
+    public void updateServerMap(){
+        //Take new server, and add it to the hashmap
+
+    }
+
+    public void giveServerMap(){
+        //Simply return the hashmap when called
+
+    }
+
+}
