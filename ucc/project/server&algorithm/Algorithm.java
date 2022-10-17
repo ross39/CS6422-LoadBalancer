b5f073cfa7b4a426d3da6f286997499c38c99f02
hmmmq <122110296@umail.ucc.ie>
2022-10-17 10:37:13 +0100

    Server api, Algorithm api
    I initialize the Server class and its API. It also has a main method that serves as a test that can run successfully. Meanwhile, the Algorithm class and its API have also been created.

---
+++ ucc/project/server&algorithm/Algorithm.java
@@ -0,0 +1,24 @@
+import java.util.List;
+
+/**
+ * @author Meiqi Huang
+ */
+public class Algorithm {
+    /**
+     * choose a server from server list use roundrobin algorithm
+     * @param serverList
+     * @return the server's ip
+     */
+    public static String RoundRobin(List<Server> serverList){
+        return null;
+    }
+
+    /**
+     * choose a server from server list use WeightDependent algorithm
+     * @param serverList
+     * @return the server's ip
+     */
+    public static String WeightDependent(List<Server> serverList){
+        return null;
+    }
+}
