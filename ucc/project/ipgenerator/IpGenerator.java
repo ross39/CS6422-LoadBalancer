b99df00b5461c34c74a766247221f46f60adc779
donut <sunruyun2@gmail.com>
2022-10-20 21:41:31 +0100

    initialize ipgenerator

---
+++ ucc/project/ipgenerator/IpGenerator.java
@@ -0,0 +1,15 @@
+package ucc.project.ipgenerator;
+import java.io.File;
+
+/**
+ * 
+ * A IPGenerator class that generate ipAddress. use the path of the server file to stimulate IP address
+ * @author Ruyun
+ */
+public class IpGenerator {
+
+    public static void getIP(){
+
+    }
+
+}
