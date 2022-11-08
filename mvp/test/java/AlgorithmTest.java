a873e3256eacc59fcd2dd05cac36e538825d797a
rossheaney <Rheaney26@gmail.com>
2022-11-08 17:56:32 +0000

    Added test classes, changed author comments to reflect group

--- mvp/test/java/AlgorithmTest.java
+++ mvp/test/java/AlgorithmTest.java
@@ -6,6 +6,7 @@ package test.java;
 import java.util.ArrayList;
 
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -40,6 +41,40 @@ public class AlgorithmTest {
         algo.setServerList(testListExpected);
         assertArrayEquals(algo.getServerList().toArray(), testListExpected.toArray());
     }
+
+    void testRandomSelectNext(){
+        //This class seems to pick a random server, not sure how to test this as its random
+        // you could assertFalse but this seems gimmicky as the test is guaranteed to fail 1/x times where x is size of severlist
+        // if you assertTrue the test is guaranteed to fail 1 - 1/x times 
+    }
+
+    @Test
+    void testRoundRobinNext() {
+        //Round robin works by assigning time slices to each process in equal portions
+        //and in circular order, handling all processes without priority
+       ArrayList<Server> testListExpected = new ArrayList<>();
+       testListExpected.add(new Server(1, "ip2"));
+       testListExpected.add(new Server(2, "ip4"));
+       testListExpected.add(new Server(3, "ip6"));
+       testListExpected.add(new Server(5, "ip7"));
+
+       algo.setServerList(testListExpected);
+       algo.RoundRobinNext();
+       assertThat( algo.RoundRobinNext(), testListExpected.get(0));
+
+
+
+
+
+
+    }
+
+    @Test
+    void testWeightDependentNext() {
+      //
+    }
+
+
 
 
 }
