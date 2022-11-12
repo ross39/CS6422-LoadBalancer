26c5a082c032555e29f83346c83f02c3a80ae112
Cheuk Wei Lin <cheukweilin@pc-5-90.customer.ask4.lan>
2022-11-12 00:17:47 +0000

    Add initial testing

--- mvp/test/java/ClientTest.java
+++ mvp/test/java/ClientTest.java
@@ -1,3 +1,30 @@
+package test.java;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.junit.jupiter.api.Test;
+
 public class ClientTest {
+
+    @Test
+    public static void testReadFile() throws IOException {
+
+        String test = "Tom";
+
+        String request;
+        // BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader input = new BufferedReader(new FileReader("mvp/test/java/clientrequests.txt"));
+        while ((request = input.readLine()) != null) {
+            System.out.println(request);
+            input.close();
+        }
+
+        assertEquals(test, request);
+    }
     
 }
