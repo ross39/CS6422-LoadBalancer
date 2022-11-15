45c58122f6d006c5a828d500ee189f2061840b4a
donut <sunruyun2@gmail.com>
2022-11-15 20:15:24 +0000

    initialize ip generator

---
+++ mvp/test/java/IpGeneratorTest.java
@@ -0,0 +1,12 @@
+package test.java;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class IpGeneratorTest {
+    @Test
+    void initializationTest(){
+        assertEquals(0, 0);   
+    }
+}
