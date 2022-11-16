d9f89084426d5a40c50241cfc501cb03a9338a91
Cheuk Wei Lin <cheukweilin@pc-5-90.customer.ask4.lan>
2022-11-16 19:51:57 +0000

    Add test to read multiple lines of requests

--- mvp/test/java/ClientTest.java
+++ mvp/test/java/ClientTest.java
@@ -6,8 +6,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.junit.jupiter.api.Test;
-import main.java.Client;
 
 public class ClientTest {
 
@@ -15,21 +19,34 @@ public class ClientTest {
     @Test
     public void testReadFile() throws IOException {
 
-        String test = "Tom";
-        String request;
+        String expectedResult = "Tom";
+        String actualResult;
 
         InputStream file = getClass().getResourceAsStream("clientrequest.txt");
         BufferedReader input = new BufferedReader(new InputStreamReader(file));
-        request = input.readLine();
+        actualResult = input.readLine();
 
-        assertEquals(test, request);
-        
+        assertEquals(expectedResult, actualResult);
+
         input.close();
     }
 
     // Test for multiple lines in a file
     @Test
     public void testReadFileMultipleLines() throws IOException {    
-    
+        
+        List<String> expectedResult = Arrays.asList("Tom", "Sam", "Tim", "May");
+        
+        // BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        InputStream file = getClass().getResourceAsStream("multipleclientrequests.txt");
+        BufferedReader input = new BufferedReader(new InputStreamReader(file));
+
+        List<String> actualResult = input.lines().collect(Collectors.toList());
+
+        System.out.println(actualResult);
+        
+        assertEquals(expectedResult, actualResult);
+        input.close();
+
     }
 }
