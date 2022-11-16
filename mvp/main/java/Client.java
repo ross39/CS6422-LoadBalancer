eeb70dccfa36adc8ddfb20c1068b0ef8e45a8ab8
hmmmq <122110296@umail.ucc.ie>
2022-11-16 22:42:42 +0000

    code review
    code has been reviewed and revised. three test passed.

--- mvp/main/java/Client.java
+++ mvp/main/java/Client.java
@@ -1,26 +1,36 @@
 package main.java;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Client {
     
     private static String request;
 
-    public static void main(String[] args) {
-       readFile();
+    public static List<String> actualResult;
+
+    public static void main() {
+        String path = "main/java/resource/multipleclientrequests.txt";
+        readFile(path);
     }
 
-    public static void readFile() {
+    public static void readFile(String filepath) {
 
         try {
-           /* BufferedReader to read text from an input stream */
-           BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 
-           /* While loop to read each line of the file. request - each line is a different client request */ 
+            InputStream file = new FileInputStream(new File(filepath));
+
+            BufferedReader input = new BufferedReader(new InputStreamReader(file));
+
+            // for test
+            actualResult = input.lines().collect(Collectors.toList());
+
+           /* While loop to read each line of the file. request - each line is a different client request */
            while ((request = input.readLine()) != null) {
             System.out.println("Client request: " + request);
            }
+
 
            /* Closes input */
            input.close();
--- mvp/test/java/ClientTest.java
+++ mvp/test/java/ClientTest.java
@@ -6,11 +6,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-
+import main.java.Client;
 import org.junit.jupiter.api.Test;
 
 public class ClientTest {
@@ -22,7 +21,7 @@ public class ClientTest {
         String expectedResult = "Tom";
         String actualResult;
 
-        InputStream file = getClass().getResourceAsStream("clientrequest.txt");
+        InputStream file = getClass().getResourceAsStream("resource/clientrequest.txt");
         BufferedReader input = new BufferedReader(new InputStreamReader(file));
         actualResult = input.readLine();
 
@@ -38,7 +37,7 @@ public class ClientTest {
         List<String> expectedResult = Arrays.asList("Tom", "Sam", "Tim", "May");
         
         // BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-        InputStream file = getClass().getResourceAsStream("multipleclientrequests.txt");
+        InputStream file = getClass().getResourceAsStream("resource/multipleclientrequests.txt");
         BufferedReader input = new BufferedReader(new InputStreamReader(file));
 
         List<String> actualResult = input.lines().collect(Collectors.toList());
@@ -48,5 +47,12 @@ public class ClientTest {
         assertEquals(expectedResult, actualResult);
         input.close();
 
+    }
+
+    @Test
+    void testClient(){
+        List<String> expectedResult = Arrays.asList("Tom", "Sam", "Tim", "May");
+        Client.readFile("main/java/resource/multipleclientrequests.txt");
+        assertEquals(expectedResult, Client.actualResult);
     }
 }
