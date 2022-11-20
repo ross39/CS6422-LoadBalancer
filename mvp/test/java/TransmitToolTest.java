4354a0c62395c6aff39ab91a05c0702e4000d8d9
yiqiu <122100074@umail.ucc.ie>
2022-11-20 21:31:13 +0000

    Update TransmitTool Test.

--- mvp/test/java/TransmitToolTest.java
+++ mvp/test/java/TransmitToolTest.java
@@ -3,33 +3,39 @@ package test.java;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import main.java.Request;
+import main.java.TransmitTool;
+
+import java.io.File;
+
 class TransmitToolTest {
 
     @Test
-    void processValidMessage() {
+    void generateNewFile() {
+
         TransmitTool transmitTool = new TransmitTool();
+
         String token = transmitTool.createToken(2, "Abc123456");
+        String token2 = transmitTool.createToken(3, "Abc123456");
+        String token3 = transmitTool.createToken(10, "Abc123456");
+
         Request R1 = new Request(2,"10.0.0.1", "10.0.0.2 Batch1/Stream1", token);
+        Request R2 = new Request(3,"10.0.0.1", "10.0.0.20 Batch2/Stream6", token2);
+        Request R3 = new Request(10,"10.0.0.1", "10.0.0.50 Batch1/Stream9", token3);
+
         transmitTool.processMessage(R1);
-
-        assertNotNull(transmitTool.json);
-        assertEquals(true, transmitTool.pass);
-
-    }
-
-    @Test
-    void processInvalidMessage() {
-        TransmitTool transmitTool = new TransmitTool();
-        Request R2 = new Request(2,"10.0.0.1", "10.0.0.2 Batch1/Stream1", "Abc123");
         transmitTool.processMessage(R2);
+        transmitTool.processMessage(R3);
 
-        assertNull(transmitTool.json);
-        assertEquals(false, transmitTool.pass);
+        transmitTool.sendToServer();
 
-    }
+        File f1 = new File("2.json");
+        File f2 = new File("3.json");
+        File f3 = new File("10.json");
 
-    @Test
-    void sendToServer() {
+        assertEquals(true, f1.exists());
+        assertEquals(true, f2.exists());
+        assertEquals(true, f3.exists());
 
     }
 }
