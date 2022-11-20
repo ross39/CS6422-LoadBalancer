7b52782bde17beacdb35676ded9e1ddd46139259
donut <sunruyun2@gmail.com>
2022-11-20 18:18:21 +0000

    add basic ip generator test

--- mvp/test/java/IpGeneratorTest.java
+++ mvp/test/java/IpGeneratorTest.java
@@ -4,9 +4,21 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
+import main.java.IpGenerator;
+import main.java.Server;
+
 public class IpGeneratorTest {
+
     @Test
     void initializationTest(){
-        assertEquals(0, 0);   
+        String address1 = IpGenerator.getIP();
+        assertEquals(address1, "mvp/out/server1.txt");
+    }
+
+    @Test
+    void checkFileAddress(){
+        Server server1 = new Server('1',"mvp/out/server2.txt");
+        String ipAddress = server1.getIp();
+        assertEquals(ipAddress, "mvp/out/server2.txt");
     }
 }
