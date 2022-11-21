0527f374d6126a4219a6818e32f144885c3f8f69
donut <31880770+sunruyun2@users.noreply.github.com>
2022-11-21 10:56:15 +0000

    Ipgenerator (#20)
    * initialize ip generator
    
    * add basic ip generator test
    
    * revise complete
    
    change file store path
    add integration test with server
    
    * fixed incompatible path problem
    
    * file path fixed, deleted some test server text file
    
    * revised
    
    * revised
    
    * file path fixed
    
    Co-authored-by: hmmmq <122110296@umail.ucc.ie>

--- mvp/main/java/IpGenerator.java
+++ mvp/main/java/IpGenerator.java
@@ -1,29 +1,66 @@
 package main.java;
+
 import java.io.File;
+import java.io.IOException;
 
 /**
- * 
  * A IPGenerator class that generate ipAddress. use the path of the server file to stimulate IP address
+ *
  * @author Ruyun
  */
 public class IpGenerator {
 
+    private int i = 1;
+
+    private String filePath = "main/java/resource/server/";
+
+    private String fileNamePrefix = "server" ;
+
+    private String fileNameSuffix = ".txt";
+
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    private static IpGenerator ipGenerator = new IpGenerator();
+
+    private IpGenerator() {
+    }
+
+    public static IpGenerator getInstance() {
+
+        return ipGenerator;
+
+    }
+
     /**
      * generate a IP address and return it (followed as server1, server2, server3)
      * server is stored in out repository now
+     *
      * @return String
      */
-    public static String getIP(){
-        Integer i = 1;
-        String path = "mvp/out/server" + i.toString() + ".txt";
-        File f = new File(path);
-        while (f.exists()){
-            i++;
-            path = "mvp/out/server" + i.toString() + ".txt";
-            f = new File(path);
-        }
-        return path;
-    }
+    public String getIP() {
 
+        String fileName = filePath + fileNamePrefix + i + fileNameSuffix;
+
+        File file = new File(fileName);
+
+        while (file.exists()) {
+
+            i++;
+
+            fileName = filePath + fileNamePrefix + i + fileNameSuffix;
+
+            file = new File(fileName);
+
+        }
+
+        return fileName;
+    }
 
 }
