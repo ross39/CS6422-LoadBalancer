a8ef67b6da6f3e830c1ac6c385aebee7f2e5e36d
hmmmq <122110296@umail.ucc.ie>
2022-11-21 21:01:31 +0000

    reformat

--- mvp/test/java/TransmitToolTest.java
+++ mvp/test/java/TransmitToolTest.java
@@ -2,12 +2,11 @@ package test.java;
 
 import main.java.*;
 import org.junit.jupiter.api.Test;
+
 import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import static main.java.Client.PATH;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TransmitToolTest {
@@ -20,31 +19,31 @@ class TransmitToolTest {
 
     List<String> clientinfo = new ArrayList<>();
 
-    void prepareFileEnvironment(){
+    void prepareFileEnvironment() {
 
-            // create server file
+        // create server file
 
-             server1 = new Server(1);
+        server1 = new Server(1);
 
-             server2 = new Server(3);
+        server2 = new Server(3);
 
-             LoadBalancer.getInstance().setServer_list(ServerPool.getServerPool().getPool());
+        LoadBalancer.getInstance().setServer_list(ServerPool.getServerPool().getPool());
 
-             Client.readFile("main/java/resource/clientinfo.txt");
+        Client.readFile("main/java/resource/clientinfo.txt");
 
-             clientinfo = Client.actualResult;
+        clientinfo = Client.actualResult;
 
     }
 
-    void prepareSocket(){
+    void prepareSocket() {
 
         List<Socket> socketList = new ArrayList<>();
 
-        for (int i = 0 ; i < clientinfo.size() ; i++){
+        for (int i = 0; i < clientinfo.size(); i++) {
 
             Server next = LoadBalancer.getInstance().getNext();
 
-            Socket socket = new Socket( clientinfo.get(i), next );
+            Socket socket = new Socket(clientinfo.get(i), next);
 
             socketList.add(socket);
 
@@ -61,9 +60,9 @@ class TransmitToolTest {
 
         prepareSocket();
 
-        assertEquals(0, new File(server1.getIp()).length());
+        assertEquals(true, (new File(server1.getIp()).length()) == 0);
 
-        assertEquals(0, new File(server2.getIp()).length());
+        assertEquals(true, (new File(server2.getIp()).length()) == 0);
 
         assertEquals(false, transmitTool.getSockets().isEmpty());
 
