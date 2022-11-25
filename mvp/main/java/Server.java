6c805bcfbdbc10d4023a939e29e9c1c20dd26182
hmmmq <122110296@umail.ucc.ie>
2022-11-25 07:42:19 +0000

    Update Server.java

--- mvp/main/java/Server.java
+++ mvp/main/java/Server.java
@@ -1,12 +1,28 @@
 package main.java;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 /*
  * Authors Cheuk Wei Lin, Meiqi Huang,Ross Heaney, Ruyun Sun, Yiqiu Wang
  */
 
 public class Server implements Comparable {
+
+    private List<String> requests = new LinkedList<>();
+
+    //the amount of request that the server can process =  weight * factor;
+    private int factor = 2;
+
+    private boolean server_switch = true;
+
+    private boolean server_off = false;
+
 
     /**
      * initialize the ip adrress and default weight once start.
@@ -21,6 +37,8 @@ public class Server implements Comparable {
         // register itself to serverpool once created new object
         register();
 
+        startListen();
+
     }
 
     public Server(int weight, String ip) {
@@ -32,6 +50,8 @@ public class Server implements Comparable {
         // register itself to serverpool once created new object
         register();
 
+        startListen();
+
     }
 
 
@@ -41,7 +61,7 @@ public class Server implements Comparable {
      *
      * @param weight
      */
-    Server(int weight) {
+    public Server(int weight) {
 
         this.ip = getIPAddress();
 
@@ -49,6 +69,8 @@ public class Server implements Comparable {
 
         // register itself to serverpool once created new object
         register();
+
+        startListen();
     }
 
     /**
@@ -58,9 +80,8 @@ public class Server implements Comparable {
      */
     private String getIPAddress() {
 
-        String ip = getFakeIpAddress();
+        return IpGenerator.getInstance().getIP();
 
-        return ip;
     }
 
     /**
@@ -69,7 +90,9 @@ public class Server implements Comparable {
      * @return
      */
     private String getFakeIpAddress() {
-        return IpGenerator.getInstance().getIP();
+
+        return "server1.txt";
+
     }
 
     /**
@@ -104,10 +127,14 @@ public class Server implements Comparable {
 
         }
 
+        ProcessingRequest();
+
     }
 
     public String getIp() {
+
         return ip;
+
     }
 
     public void setIp(String ip) {
@@ -143,13 +170,154 @@ public class Server implements Comparable {
         return 0;
     }
 
-    /**
-     * test a server
-     *
-     * @param args
-     */
-    public static void main(String[] args) {
-        Server server = new Server();
-        server.startListen();
+    public synchronized List<String> getRequests() {
+
+        return requests;
+    }
+
+    public synchronized Boolean push(String clientinfo){
+
+        if (requests.size() <= weight*factor){
+
+            requests.add(clientinfo);
+
+            return true;
+
+        }
+        else{
+
+            return false;
+
+        }
+    }
+
+    private void ProcessingRequest(){
+
+        Server server = this;
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                while (server_switch){
+
+                    if (requests.isEmpty()) {
+
+                        System.out.println("\n----Server Thread: Server "+server.ip+" is waiting for request...");
+
+                        // sleep
+                        try {
+
+                            Thread.sleep(3000);
+
+                        } catch (InterruptedException e) {
+
+                            e.printStackTrace();
+
+                        }
+
+
+                    }else{
+
+
+                        while (!requests.isEmpty()){
+
+                            Socket socket = new Socket(requests.get(0), server);
+
+                            ProcessRequestOneByOne(socket);
+
+                            requests.remove(0);
+
+                            // delay
+                            try {
+
+                                Thread.sleep(1000);
+
+                            } catch (InterruptedException e) {
+
+                                e.printStackTrace();
+
+                            }
+
+                        }
+
+                    }
+
+
+
+                }
+
+                System.out.println("\n----Server Thread: Server "+server.ip+" is closed");
+
+                server_off = true;
+            }
+        });
+
+        thread.start();
+
+
+
+
+    }
+
+    private void ProcessRequestOneByOne(Socket socket) {
+
+        String clientip = socket.getClientip();
+
+        String message = socket.getMessage();
+
+        String serverip = socket.getServerip();
+
+        String clientinfo = socket.getClientinfo();
+
+        File file = new File(serverip);
+
+        System.out.println("\n----Server Thread: Server "+serverip+" processing the request "+clientinfo+"!");
+
+        try {
+
+            FileOutputStream fileOutputStream = new FileOutputStream(file, true);
+
+            if (clientinfo.isEmpty()){
+
+                String s = clientip + " : " + message+"\n";
+
+                fileOutputStream.write(s.getBytes());
+
+
+            }else{
+
+                String s = clientinfo + "\n";
+
+                fileOutputStream.write(s.getBytes());
+
+            }
+
+            System.out.println("\n----Server Thread: Server "+serverip+" complete processing "+clientinfo+"\n");
+
+            fileOutputStream.close();
+
+
+        } catch (IOException e) {
+
+            System.out.println("Server shut down or not exist");
+        }
+
+
+
+
+
+    }
+
+    public boolean isServer_switch() {
+        return server_switch;
+    }
+
+    public void setServer_switch(boolean server_switch) {
+        this.server_switch = server_switch;
+    }
+
+    public boolean isServer_off() {
+        return server_off;
     }
 }
